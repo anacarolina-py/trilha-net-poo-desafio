d394fa3b17d263bbb62772e63d34c7f7fad4abb3
Ana Carolina <anacarolinaalvez_@hotmail.com>
2025-09-26 12:30:17 -0300

    Update Program.cs
    Código modificado conforme o enunciado, para resolução do exercício.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+// Criando um Nokia
+Smartphone nokia = new Nokia("12345", "Nokia Tijolão", "111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+// Criando um iPhone
+Smartphone iphone = new Iphone("67890", "iPhone 15", "222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
